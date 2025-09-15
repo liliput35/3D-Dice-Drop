5bb4b3f04832ac7b7136f1ad00f4df5d6c7a5e08
Lorenz Ciocon <lorenzciocon@gmail.com>
2025-09-15 22:22:36 +0800

    dice game

---
+++ Dice Drop/Assets/diceController.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class diceController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool isDropped = false;
+
+    public Text sideupText ; 
+
+    public GameObject side1;
+    public GameObject side2;
+    public GameObject side3;
+    public GameObject side4;
+    public GameObject side5;
+    public GameObject side6;
+
+    public int currentSide = 0;
+    private Vector3 startPosition = Vector3.zero;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+
+        startPosition = transform.position;
+    }
+
+    public void RotateDice(){
+        float randX = Random.Range(100f, 360f) ; 
+        float randY = Random.Range(100f, 360f) ; 
+        float randZ = Random.Range(100f, 360f) ; 
+
+        transform.rotation = Quaternion.Euler(randX, randY, randZ) ;
+
+    }
+
+    public void DropDice(InputAction.CallbackContext context)
+    {
+        if (context.performed && !isDropped)
+        {
+            RotateDice() ;
+            rb.useGravity = true;
+            isDropped = true;
+        } else if (context.performed && isDropped)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            transform.position = startPosition;
+            transform.rotation = Quaternion.identity; 
+            rb.useGravity = false;
+            isDropped = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isDropped && rb.IsSleeping()) 
+        {
+            float maxY = float.MinValue;
+            int sideUp = 0;
+
+            GameObject[] sides = { side1, side2, side3, side4, side5, side6 };
+
+            for (int i = 0; i < sides.Length; i++)
+            {
+                float y = sides[i].transform.position.y;
+                if (y > maxY)
+                {
+                    maxY = y;
+                    sideUp = i + 1; 
+                }
+            }
+
+
+            
+            currentSide = sideUp;
+            sideupText.text = currentSide + "";
+        }
+    }
+}
